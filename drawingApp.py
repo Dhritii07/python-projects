b5fdd2ce8a6ed594ad95c323d3917ac08685fa90
Dhritii07 <dhritivaidya11@gmail.com>
2025-07-08 00:11:28 +0530

    art indeed

---
+++ drawingApp.py
@@ -0,0 +1,52 @@
+'''
+features:
+- brush selection
+- brush size selection
+- eraser selection
+- eraser size selection
+- multiple color selections for background and drawing as well
+- clear the drawing 
+- saving the image
+'''
+
+import time
+from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
+import PIL.ImageGrab as ImageGrab  #used for grabbing ss of selected portion
+from tkinter import colorchooser, messagebox
+
+#main
+window = Tk()
+window.title('Drawing App')
+
+def select_brush():
+    pass
+
+def change_brush_size():
+    pass
+
+def select_brush_size():
+    pass
+
+def select_eraser():
+    pass
+
+def select_eraser_size():
+    pass
+
+def select_brush_color():
+    pass
+
+def select_background_color():
+    pass
+
+def clear_canavs():
+    pass
+
+def save_image():
+    pass
+
+#menu bar
+menu_bar = Menu(window)
+
