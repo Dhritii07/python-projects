e3ea752763acaca58be264443e7ec041d88ffc97
Dhritii07 <dhritivaidya11@gmail.com>
2025-07-10 00:25:56 +0530

    brush functions

--- drawingApp.py
+++ drawingApp.py
@@ -16,15 +16,17 @@ from tkinter import filedialog
 import PIL.ImageGrab as ImageGrab  #used for grabbing ss of selected portion
 from tkinter import colorchooser, messagebox
 
-#main
+#main: new tkinter window
 window = Tk()
 window.title('Drawing App')
 
 def select_brush():
-    pass
+    global selected_color
+    selected_color = previous_color 
 
 def change_brush_size():
-    pass
+    global brush_size 
+    brush_size = int(value)
 
 def select_brush_size():
     pass
@@ -50,3 +52,37 @@ def save_image():
 #menu bar
 menu_bar = Menu(window)
 
+#brush menu
+brush_menu = Menu(menu_bar, tearoff=0)
+brush_menu.add_command(label='Select Brush', command=select_brush)
+brush_menu.add_command(label='Select Brush Size', command=select_brush_size)
+
+#eraser menu
+eraser_menu = Menu(menu_bar, tearoff=0)
+eraser_menu.add_command(label='Select Eraser', command=select_eraser)
+brush_menu.add_command(label='Select Eraser Size', command=select_eraser_size)
+
+#color menu
+color_menu = Menu(menu_bar, tearoff=0)
+color_menu.add_command(label='Select Drawing Color', command=select_brush_color)
+color_menu.add_command(label='Select Background Color', command=select_background_color)
+
+#clear menu
+clear_menu = Menu(menu_bar, tearoff=0)
+clear_menu.add_command(label='Clear Canvas', command=clear_canavs)
+
+#save 
+save_menu = Menu(menu_bar, tearoff=0)
+save_menu.add_command(label='Save Drawing', command=save_image)
+
+#menu bar
+menu_bar.add_cascade(label='Brush', menu=brush_menu)
+menu_bar.add_cascade(label='Eraser', menu=eraser_menu)
+menu_bar.add_cascade(label='Color', menu=color_menu)
+menu_bar.add_cascade(label="Clear", menu=clear_menu)
+menu_bar.add_cascade(label="Save", menu=save_menu)
+
+window.config(menu=menu_bar)
+canvas = Canvas(window, width=800, height=480, bg='white')
+canvas.pack()
+window.mainloop()
