5163599f432e6702d555eb3fb313b18eea7fc799
Dhritii07 <dhritivaidya11@gmail.com>
2025-10-24 01:40:21 +0530

    guess

---
+++ guessNum.py
@@ -0,0 +1,31 @@
+import random
+
+def guessNum():
+    print('Why are you here?')
+    print('Nvm')
+    print('Think of a number between 1 and 1000')
+    print('Note: Cold is for Too Low and Hot is for Too High')
+    num = random.randint(1,1000)
+
+    attempts = 0
+    correct = False
+
+    while not correct and attempts <= 3:
+        guess = input('Enter your guess: ')
+
+        if not guess.isdigit():
+            print('Please enter a valid number.')
+            continue
+
+        guess = int(guess)
+        attempts += 1
+
+        if guess < num:
+            print('Cold')
+        elif guess > num:
+            print('Hot')
+        else:
+            print(f"Congrats! I guess you have guessed the number in {attempts} attempts!")
+            correct = True
+
+guessNum()
